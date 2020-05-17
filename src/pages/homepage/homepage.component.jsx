c77b864fc65b3b1e6ea1981a6b92e53186230271
Kinkar <rkinkar@dxc.com>
2020-05-17 20:53:05 +0530

    broken homepage into components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component.jsx';
+
+const HomePage = () =>(
+    <Directory />
+    )
+
+export default HomePage;
